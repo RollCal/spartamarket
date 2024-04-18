23686bd57ec6506bbd301c5952800e8dcadf228a
Jungho Lee <awjdgh1111@gmail.com>
2024-04-18 13:12:00 +0900

    FBV로 블로그 포스트 목록페이지

--- post/views.py
+++ post/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('-pk') # DB에 쿼리를 날려 원하는 레코드 가져오기, orderby로 최신 포스트(personal_key의 역순)로 정렬.
+
+    return render(request, 'index.html',
+                   {
+                      'posts': posts
+                     }
+                  )
