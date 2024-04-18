ff03ac2a9eaddc68969363efd846d6f4e82dde41
Jungho Lee <awjdgh1111@gmail.com>
2024-04-18 15:19:16 +0900

    CBV로 포스트 목록화 시키기

--- post/urls.py
+++ post/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('<int:pk>/', views.single_post_page, name='single_post_page'),
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', views.PostList.as_view()), # 디폴트 페이지가 Postlist를 바로 보여주도록 수정
 ]
--- post/views.py
+++ post/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 
+class PostList(ListView):
+    model = Post
+    template_name = 'index.html' # 이걸 안적으면 post_list.html을 템플릿으로 인식해버림
+    ordering = '-pk' # 최신순 정렬
+
+
+
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk') # DB에 쿼리를 날려 원하는 레코드 가져오기, orderby로 최신 포스트(personal_key의 역순)로 정렬.
-
-    return render(request, 'index.html',
-                   {
-                      'posts': posts
-                     }
-                  )
-
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk') # DB에 쿼리를 날려 원하는 레코드 가져오기, orderby로 최신 포스트(personal_key의 역순)로 정렬.
+#
+#     return render(request, 'index.html',
+#                    {
+#                       'posts': posts
+#                      }
+#                   )
+#
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
 
